7e3a92decc508777a147c733d216c93b8d497460
zhaozg <1067557360@qq.com>
2019-05-09 16:55:13 +0800

    two commit

---
+++ spring-cloud-client/src/main/java/com/spring/cloud/Application.java
@@ -0,0 +1,29 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class Application {
+	public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+	
+	@Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+		HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpRequestFactory.setReadTimeout(5000);
+		httpRequestFactory.setConnectTimeout(5000);
+		return new RestTemplate(httpRequestFactory);
+    }
+
+}
